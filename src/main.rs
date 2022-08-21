8d26d02076d7ddbf425394564afbe1a34977eb46
nerioalfonso <nerioalfonso@nerio-PC.Home>
2022-08-21 16:48:43 -0400

    eliminando por id y por like

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,17 @@ fn main() {
     use self::models::{Post, NewPost, PostTitleAndSlug};
     use self::schema::posts;
     use self::schema::posts::dsl::*;
-
-    //let new_post = NewPost{title: "primera publicación", body: "lelelaloli", slug: "primera-publicacion"};
-
-    //let post: Post =diesel::insert_into(posts::table).values(&new_post).get_result(&conn).expect("la inserción falló");
-
-    //Un solo atributo
-    /* let post_update = diesel::update(
-        posts.filter(id.eq(4)))
-            .set(slug.eq("lexlaloxx"))
-    .get_result::<Post>(&conn).expect("Error al actualizar");*/
-    //Multiples atributos
-    let post_update = diesel::update(
-        posts.filter(id.eq(4)))
-            .set((slug.eq("lexlaloxx2"), body.eq("cuerpotx 2")))
-    .get_result::<Post>(&conn).expect("Error al actualizar");
-
+    //eliminar por id
+    let post_update = diesel::delete(
+        posts.filter(id.eq(4))).execute(&conn).expect("Error al eliminar");
+    //eliminar por like
+    let post_update = diesel::delete(
+        posts.filter(slug.like("%publicacion%"))).execute(&conn).expect("Error al eliminar");
     //query completo
     let posts_result =posts.load::<Post>(&conn).expect("error al obtener los datos");
     println!("{}", "query completo");
     for post in posts_result 
     {
         println!("{:?}", post);
-    }
-    
+    }   
 }
