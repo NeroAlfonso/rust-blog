0380801a541d7dd158d600dc4a06d1c94e2d63d9
nerioalfonso <nerioalfonso@nerio-PC.Home>
2022-08-21 15:29:57 -0400

    agregado uso de macros al main, cargadas variables del .env, establecida conexión

--- migrations/2022-08-21-182200_rust_blog/up.sql
+++ migrations/2022-08-21-182200_rust_blog/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+CREATE TABLE posts
+(
+    id serial primary key,
+    title varchar not null,
+    slug varchar not null,
+    body text not null
+)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use] //para el esquema
+extern crate diesel;
+use dotenv::dotenv;
+use std::env;
+use diesel::pg::PgConnection;
+use diesel::prelude::*; //?
 fn main() {
+    dotenv().ok();
+    let db_url : String =env::var("DATABASE_URL").expect("db url variable no encontrada");
+    PgConnection::establish(&db_url).expect("Imposible conectar a postgres");
+    println!("{}", db_url);
     println!("Hello, world!");
 }
