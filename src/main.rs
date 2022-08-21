b95fd9cc73f00d70fd102a487850ad73cbebad7e
nerioalfonso <nerioalfonso@nerio-PC.Home>
2022-08-21 15:42:26 -0400

    consulta de registro, primeros modelos para inserción

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ use dotenv::dotenv;
 use std::env;
 use diesel::pg::PgConnection;
 use diesel::prelude::*; //?
+pub mod schema;
+pub mod models;
 fn main() {
     dotenv().ok();
     let db_url : String =env::var("DATABASE_URL").expect("db url variable no encontrada");
-    PgConnection::establish(&db_url).expect("Imposible conectar a postgres");
-    println!("{}", db_url);
-    println!("Hello, world!");
+    let conn : PgConnection = PgConnection::establish(&db_url).expect("Imposible conectar a postgres");
+    use self::models::Post;
+    use self::schema::posts::dsl::*;
+    let posts_result =posts.load::<Post>(&conn).expect("error al obtener los datos");
+    for post in posts_result 
+    {
+        println!("{}", post.title);
+    }
 }
---
+++ src/models.rs
@@ -0,0 +1,16 @@
+#[derive(Queryable)] //esto se puede convertir en un row
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub slug: String,
+    pub body: String
+}
+use super::schema::posts;
+#[derive(Insertable)] //macro
+#[table_name="posts"] //macro
+pub struct NewPost<'a> //poder utilizar los std dentro de las estructuras (lifetime)
+{
+    pub title &'a str,
+    pub body &'a str,
+    pub slug &'a str
+}
