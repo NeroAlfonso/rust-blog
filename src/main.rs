082a69b32b9e8e78711ae421823fd09172f63a02
nerioalfonso <nerioalfonso@nerio-PC.Home>
2022-08-21 22:15:00 -0400

    vistas y navegación

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,6 @@ async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> i
     println!("{:?}", item);
     let conn =pool.get().expect("Error al traer una conexión");
 
-    let new_post = NewPost{
-        title: "wm",
-        slug: "kdsadkk",
-        body: "dskdlsak"
-    };
-
     match web::block(move ||{Post::create_post(&conn, &item)}).await {
         Ok(data) => 
         {
@@ -56,12 +50,49 @@ async fn load_posts(pool: web::Data<DbPool>) -> impl Responder
     }
 }
 
-#[get("/")]
-async fn index(template_manager: web::Data<tera::Tera>) -> impl Responder
+#[get("/post/{blog_slug}")]
+async fn get_post(
+    pool: web::Data<DbPool>, 
+    template_manager: web::Data<tera::Tera>, 
+    blog_slug: web::Path<String>) -> impl Responder
 {
-    let mut ctx = tera::Context::new();
-    HttpResponse::Ok().content_type("text/html")
-        .body(template_manager.render("index.html", &ctx).unwrap())
+    let conn =pool.get().expect("Error al traer una conexión");
+    let url_slug =blog_slug.into_inner();
+    match web::block(move ||{posts.filter(slug.eq(url_slug)).load::<Post>(&conn)}).await {
+        Ok(data) => 
+        {
+            let data = data.unwrap();
+            if data.len() ==0
+            {
+                return HttpResponse::NotFound().finish();
+            }
+            let data = &data[0];
+            let mut ctx = tera::Context::new();
+            ctx.insert("post", data);
+            HttpResponse::Ok().content_type("text/html")
+                .body(template_manager.render("post.html", &ctx).unwrap())
+        },
+        Err(err) => HttpResponse::Ok().body("Error al obtener los valores")
+    }
+    
+}
+
+#[get("/")]
+async fn index(pool: web::Data<DbPool>, template_manager: web::Data<tera::Tera>) -> impl Responder
+{
+    let conn =pool.get().expect("Error al traer una conexión");
+    match web::block(move ||{posts.load::<Post>(&conn)}).await {
+        Ok(data) => 
+        {
+            let data = data.unwrap();
+            let mut ctx = tera::Context::new();
+            ctx.insert("posts", &data);
+            HttpResponse::Ok().content_type("text/html")
+                .body(template_manager.render("index.html", &ctx).unwrap())
+        },
+        Err(err) => HttpResponse::Ok().body("Error al obtener los valores")
+    }
+    
 }
 
 #[actix_web::main]
@@ -78,6 +109,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(load_posts)
             .service(new_post)
+            .service(get_post)
                 .data(pool.clone())
                 .data(tera)
     }).bind(("0.0.0.0", 9990)).unwrap().run().await
