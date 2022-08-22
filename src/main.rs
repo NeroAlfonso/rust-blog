adb4762f9613b9afcfc970b028afbaee124ca04d
nerioalfonso <nerioalfonso@nerio-PC.Home>
2022-08-21 20:11:14 -0400

    registro de post por medio de endpoint post

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,32 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use self::models::Post;
+use self::models::{Post, NewPost, NewPostHandler};
 use self::schema::posts;
 use self::schema::posts::dsl::*;
 
-#[get("/")]
-async fn index(pool: web::Data<DbPool>) -> impl Responder
+#[post("/post/new")]
+async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder
+{
+    println!("{:?}", item);
+    let conn =pool.get().expect("Error al traer una conexión");
+
+    let new_post = NewPost{
+        title: "wm",
+        slug: "kdsadkk",
+        body: "dskdlsak"
+    };
+
+    match web::block(move ||{Post::create_post(&conn, &item)}).await {
+        Ok(data) => 
+        {
+            HttpResponse::Ok().body(format!("{:?}", data))
+        },
+        Err(err) => HttpResponse::Ok().body("Error al obtener los valores")
+    }
+}
+#[get("/posts")]
+async fn load_posts(pool: web::Data<DbPool>) -> impl Responder
 {
     let conn =pool.get().expect("Error al traer una conexión");
     match web::block(move ||{posts.load::<Post>(&conn)}).await {
@@ -34,6 +54,12 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder
     }
 }
 
+#[get("/")]
+async fn index(pool: web::Data<DbPool>) -> impl Responder
+{
+    HttpResponse::Ok().body(format!("RustBlog"))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
@@ -43,6 +69,10 @@ async fn main() -> std::io::Result<()> {
     let pool = Pool::builder().build(conn).expect("Error al construir el pool de conexiones");
 
     HttpServer::new(move ||{
-        App::new().service(index).data(pool.clone())
+        App::new()
+            .service(index)
+            .service(load_posts)
+            .service(new_post)
+                .data(pool.clone())
     }).bind(("0.0.0.0", 9900)).unwrap().run().await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,20 @@
-#[derive(Queryable, Debug)] //esto se puede convertir en un row
+use serde::{Deserialize, Serialize};
+use diesel::prelude::*;
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler
+{
+    pub title: String,
+    pub body: String
+}
+
+#[derive(Queryable, Debug, Deserialize, Serialize)] //esto se puede convertir en un row
 pub struct Post {
     pub id: i32,
     pub title: String,
     pub slug: String,
     pub body: String
 }
-#[derive(Queryable, Debug)] //esto se puede convertir en un row y además poder debuggearlo
+#[derive(Queryable, Debug, Deserialize, Serialize)] //esto se puede convertir en un row y además poder debuggearlo
 pub struct PostTitleAndSlug
 {
     pub title: String,
@@ -19,4 +28,22 @@ pub struct NewPost<'a> //poder utilizar los std dentro de las estructuras (lifet
     pub title: &'a str,
     pub body: &'a str,
     pub slug: &'a str
+}
+
+impl Post {
+    pub fn slugify(title: &String) -> String 
+    {
+        title.replace(title, "-").to_lowercase()
+    }
+    pub fn create_post<'a>
+    (conn: &PgConnection, post: &NewPostHandler) -> Result<Post, diesel::result::Error>
+    {
+        let slug = Post::slugify(&post.title.clone());
+        let new_post = NewPost{
+            title: &post.title,
+            slug: &slug,
+            body: &post.body
+        };
+        diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn)
+    }
 }
